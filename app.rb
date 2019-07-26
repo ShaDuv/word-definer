d8baeefe5e27d03fcf7db6c8e941b11660c90127
Shawna Duvall <shawnaduvall@gmail.com@>
2019-07-26 10:12:53 -0700

    add individual word page and routing, plus start form and process for kids deffinitions

--- app.rb
+++ app.rb
@@ -13,6 +13,13 @@ get ('/new_word') do
   erb(:new_word)
 end
 
+get ('/word/:id') do
+  # @kid_def = Kid_Def.all
+  @word = Word.find(params[:id].to_i)
+  # @kdeffinitions = Kid_def.find_by_word(:id)
+  erb(:word)
+end
+
 post ('/new_word') do
   word = params[:word]
   definition = params[:definition]
