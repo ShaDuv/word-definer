d367a290af1d97affc398ae65d61f25b1f7aca06
Shawna Duvall <shawnaduvall@gmail.com@>
2019-07-26 08:53:33 -0700

    add the word class as well as the methods to save and view

--- lib/Word_Definer.rb
+++ lib/Word_Definer.rb
@@ -1,3 +1,24 @@
-#!/usr/bin/ruby
-  require 'pry'
-  require ''
+class Word
+  attr_accessor :word, :deff, :id
+
+  @@dictionary = {}
+  @@total_rows = 0
+
+  def initialize(attributes)
+    @word = attributes.fetch(:word).capitalize
+    @definition = attributes.fetch(:deff).capitalize
+    @id = id || @@total_rows += 1
+  end
+
+  def save
+    @@dictionary[self.id] = self
+  end
+
+  def self.all()
+    @@dictionary.values()
+  end
+
+  def self.find(id)
+    @@dictionary[id]
+  end
+end
